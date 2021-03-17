b381f00f9bcdabcee9c626190793996fbeb5d9a4
Lauren Welte <welte.lauren@gmail.com>
2021-03-17 09:52:08 -0400

    Re- wrote makeGroupedStatsDotPlot because it's use before was very unintuitive. Everything is now contained in the data structure with rows as observations and columns as conditions. Each observation can belong to a different group and each condition gets its own tick on the plot.

--- Plotting/makeGroupedStatsDotPlot.m
+++ Plotting/makeGroupedStatsDotPlot.m
@@ -1,67 +1,81 @@
-function [mean_data,std_data] = makeGroupedStatsDotPlot(data,group,trial,group_names,trial_names,cols,marker)
+function [mean_data,std_data] = makeGroupedStatsDotPlot(data,group,group_names,condition_names,cols,marker)
 
 % Make a GROUPED dot plot with the mean data and std dev on the graph. Each
-% group is a different colour and is plotted at each trial. 
+% group is a different colour and is plotted at one tick per condition. 
 
 % Outputs the mean and the standard deviation for each group.
 
-% data      =  two dimensional set of data, datapoint corresponds to row
-%               of group, data for that group is contained in the columns
+% data      =  two dimensional set of data, NXM; 
+%               N is the number of observations (there may be groups within those observations)
+%               M is the number of conditions
 
-% GIVE EACH ROW OF DATA A GROUP NUMBER AND TRIAL NUMBER
-% group     =   numbered from 1 to n (number of groups), corresponding with same rows as data
-% trial     =   numbered from 1 to n, corresponding with same rows as data
+% GIVE EACH ROW OF DATA A GROUP NUMBER
+% group         =   array that indicates each row's belonging to a group
+% Note, each condition is automatically numbered from 1 to n, corresponding 
+%     with same columns as data
 
-% EACH GROUP/TRIAL SHOULD CORRESPOND TO A GROUP NAME AND TRIAL NAME
-% group_names     = names of the groups (1,2,3 : n) in a cell array
-% trial_names = names of the groups (1,2,3 : n) in a cell array
+% EACH GROUP/CONDITION SHOULD CORRESPOND TO A GROUP NAME AND TRIAL NAME
+% group_names     = names of the groups (1,2,3 : n), strings in a cell array
+% condition_names = names of the conditions (1,2,3 : n), strings in a cell array
 
 % EACH GROUP HAS A COLOUR
 % cols       = specific colours of the groups
-
 %  marker   = marker type on the plot i.e. 'o' or 'x' or '.' etc
+
+nObs = size(data,1); % number of observations
+nCondition = size(data,2); % number of x ticks to plot at
 
 groupnums = sort(unique(group));
 ngroups = length(groupnums);
 
-trialnums = sort(unique(trial));
-ntrials = length(trialnums);
+for g = 1:ngroups
+    ind_g{g} = group == g; % figure out the indices of each group
+end
 
-nt = size(data,1);
+
+% nt = size(data,1);
 
 figure; ha = axes; hold on;
 
 if exist('cols','var')
-    c = cols;
+    col = cols;
 else
-    c = get(gca,'ColorOrder');
+    col = get(gca,'ColorOrder');
 end
 if ~exist('marker','var')
     marker = 'o';
 end
 
-for i = 1:nt
-        for dp = 1:size(data,2) % for each data point
-            if dp == 1
-                h(1,group(i)) = scatter(trial(i)-1+group(i)/(ngroups+1), data(i,dp),'markerfacecolor',c(group(i),:),'markeredgecolor',c(group(i),:),'Marker',marker);
-            else
-                scatter(trial(i)-1+group(i)/(ngroups+1), data(i,dp),'markerfacecolor',c(group(i),:),'markeredgecolor',c(group(i),:),'Marker',marker)
-            end
-        end
+for c = 1:nCondition
+   
+    for g = 1:ngroups
+        xpos = c - 1 + (g)/(ngroups*2);
+        xpos_rep = repmat(xpos, 1, sum(ind_g{g}));
+        h(g,c) = scatter(xpos_rep,data(ind_g{g},c),'marker',marker);
+        h(g,c).MarkerFaceColor = col(g,:);
+        h(g,c).MarkerEdgeColor = col(g,:);
+       
         
-        mean_data(i) = nanmean(data(i,:));
-        std_data(i) = nanstd(data(i,:));
-        temp_col = c(group(i),:)-[0.07 0.07 0.07];
+        mean_data(g,c) = nanmean(data(ind_g{g},c));
+        std_data(g,c) = nanstd(data(ind_g{g},c));
+        
+        % darken the colour for the error bar
+        temp_col = col(g,:)-[0.07 0.07 0.07];
         if sum(temp_col < 0) > 0
             temp_col(temp_col < 0) = 0;
         end
         
-        errorbar(trial(i)-1+group(i)/(ngroups+1),mean_data(i),std_data(i),'marker','square','capsize',20,'markersize',14,'color',temp_col);
-        xlim([min(trial)-1 max(trial)])
-%     end
+        errorbar(xpos,mean_data(g,c),std_data(g,c),'marker','square','capsize',20,'markersize',14,'color',temp_col);
+      xpos_save(g,c) = xpos;
+    end   
+    
+    
 end
 
-set(ha,'Xtick',trialnums-.5)
-set(ha,'Xticklabel',trial_names)
+xvals = mean(xpos_save,1);
+xlim([xvals(1)-0.5, xvals(end) + 0.5])
+
+set(ha,'Xtick',xvals)
+set(ha,'Xticklabel',condition_names)
 set(ha,'XtickLabelRotation',30)
-legend(h(1:3),group_names)
+legend(h(:,1),group_names)
