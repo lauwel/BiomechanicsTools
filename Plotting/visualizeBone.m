23bc502fbb00fc7beeb9de499d7020a9b01a9024
Lauren Welte <welte.lauren@gmail.com>
2021-04-07 11:14:49 -0400

    Changed the transparency of the edges of the bone and added an argument to specify the colour of the bone.

--- Plotting/visualizeBone.m
+++ Plotting/visualizeBone.m
@@ -1,8 +1,12 @@
-function visualizeBone(bone_pts,bone_cns,T)
+function visualizeBone(bone_pts,bone_cns,T,col)
+
+if ~exist('col','var')
+    col = [0.1 0 0.5];
+end
 
 % bone points and connections are provided with a single Transform frame to
 % plot
 
-patch('Faces',bone_cns,'vertices',transformPoints(T,bone_pts),'facealpha',0.5)
+patch('Faces',bone_cns,'vertices',transformPoints(T,bone_pts),'facecolor',col,'facealpha',0.3,'EdgeAlpha',0.3,'EdgeColor',col)
 axis equal;
 
