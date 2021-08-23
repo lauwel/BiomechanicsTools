b7b574ea9d136a66192f06a698d0985da823624a
Lauren Welte <welte.lauren@gmail.com>
2021-08-23 18:14:44 -0400

    Small functions to output the value that is greater or lesser (helpful in if statements)

---
+++ GeneralFunctions/greater.m
@@ -0,0 +1,10 @@
+function C = greater(A,B)
+% returns the value that is greater
+
+if A >= B
+    C = A;
+elseif B > A
+    C = B;
+end
+    
+    
---
+++ GeneralFunctions/lesser.m
@@ -0,0 +1,10 @@
+function C = lesser(A,B)
+% returns the value that is lesser
+
+if A <= B
+    C = A;
+elseif B < A
+    C = B;
+end
+    
+    
