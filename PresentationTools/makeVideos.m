7952ba079dd288b7818a36fd84f3de5e1fa1ef5b
Lauren Welte <welte.lauren@gmail.com>
2021-08-23 18:14:04 -0400

    Included new ways to write videos from graphs

--- PresentationTools/makeVideos.m
+++ PresentationTools/makeVideos.m
@@ -2,12 +2,12 @@ close all;
 clear
 clc
 
-vidname = 'tal cal';
+vidname = 'recoil';
 
-viddir = 'E:\ShapeModelling\Videos\Talus calc\';
+viddir = 'E:\ArchSpring_Gearing\Animation\Video_animations\arch_recoil\frames\';
 list_files = dir(fullfile(viddir,'*jpg'));
 writerObj = VideoWriter(fullfile(viddir,[vidname '.avi']));
-writerObj.FrameRate = 10;
+writerObj.FrameRate = 15;
 open(writerObj);
 % writerObj.VideoCompressionMethod
 for i = 1:length(list_files)
@@ -20,5 +20,58 @@ writeVideo(writerObj,im)
 
 end
 close(writerObj)
+
+
+
+%% make a combined x-ray, wrist viz video
+vidname = 'run';
+
+
+viddir = 'E:\Projects\ShoeLigamentSOL001B\LigamentWrapping\Videos\'; % where to save the video
+
+viddir1 = 'E:\Projects\ShoeLigamentSOL001B\LigamentWrapping\Videos\X-ray\'; % where to find the xray ims
+list_filesX = dir(fullfile(viddir1,'*tif'));
+
+viddir2 = 'E:\Projects\ShoeLigamentSOL001B\LigamentWrapping\Videos\WristViz\'; % where to find the wrist viz anims
+list_filesW = dir(fullfile(viddir2,'*jpg'));
+
+
+writerObj = VideoWriter(fullfile(viddir,[vidname '.avi']));
+writerObj.FrameRate = 15;
+open(writerObj);
+% writerObj.VideoCompressionMethod
+h= figure('Position',[1700 50 2000 1200],'Color',[1 1 1]);
+
+for i = 1:length(list_filesX)
+    subplot(1,2,1)
+    im1 = imread(fullfile(viddir1, list_filesX(i).name));
+    low = 0;
+    high = 0.3;
+    
+    K = imadjust(im1,[low high],[]);
+    imagesc(fliplr(K))
+    
+    colormap('gray')
+    axis image
+    axis off
+    
+    drawnow
+    subplot(1,2,2)
+    im2 = imread(fullfile(viddir2, list_filesW(i).name));
+    imagesc(im2)
+    axis image
+    axis off
+    
+    drawnow
+    pause(0.1)
+    
+    
+    frame = getframe(h);
+    writeVideo(writerObj,frame);
+%     writeVideo(writerObj,im)
+    
+end
+close(writerObj)
+
 
 
--- PresentationTools/makeVideosTimedWithGraphs.m
+++ PresentationTools/makeVideosTimedWithGraphs.m
@@ -3,7 +3,7 @@
 viddir = 'C:\Users\welte\Downloads\';
 
 
-xray_dir =  'E:\SOL001B\T0019_SOL001_nrun_pref_barefoot\BVR\c1_74_125_c2_74_125_ss500_250c_20190502_124204_C1S0001000UND\';
+xray_dir =  'E:\ArchSpring_Gearing\Animation\Video_animations\arch_recoil\tib cal fixed mt1\';
 
 
 
@@ -11,23 +11,27 @@ colcol = [0.4 0.4 0.4];
 
 close all;
 h = figure('Position',[50,50,1500,900]);
-hg(1) = axes(h,'Position',[0.1 0.55 0.3 0.35]);
+hg(1) = axes(h,'Position',[0.1 0.55 0.45 0.35]);
 hg(2) = axes(h,'Position',[0.1 0.1 0.3 0.35]);
 hg(3) = axes(h,'Position',[0.55 0.1 0.3 0.35]);
 him = axes(h,'Position',[0.55 0.55 0.45 0.45]);
 
 
+list_files = dir([xray_dir '*.jpg']);
+
 % get the length of the trial -> could be from frame markers
-npts = 25; % number of points
+npts = length(list_files); % number of points
 x_pts = linspace(0,100,npts); % plotting x points
 
-var1 = sin(x_pts); % variable 1
+
+var1 = ones(npts,1)*1.5; % variable 1
 var2 = sin(x_pts+pi()); % variable 2
 var3 =  cos(x_pts); % variable 3
 
 % plot 1
-plot(hg(1),x_pts,var1,'color',colcol)
-set(hg(1),'nextplot','add')
+% plot(hg(1),x_pts,var1,'color',colcol)
+set(hg(1),'nextplot','add','Xgrid','on','ygrid','on','XTick',[0:20:100]);%,'Ycolor','none')
+%%
 % plot 2
 plot(hg(2),x_pts,var2,'color',colcol)
 set(hg(2),'nextplot','add')
@@ -36,7 +40,6 @@ set(hg(2),'nextplot','add')
 plot(hg(3),x_pts,var3,'color',colcol)
 set(hg(3),'nextplot','add')
 
-list_files = dir([xray_dir '*.tif']);
 
 vid_filename = 'x-ray_graph_video.avi';
 % set up the video writing
@@ -48,7 +51,7 @@ open(writerObj);
 ct = 1; % in case you are not indexing through frames starting at 1 in the loop
 
 % plot the initial points
-h_point(1) = plot(hg(1),x_pts(ct),var1(ct),'marker','o','markerfacecolor',[0.8 0.2 0.2]);
+h_point(1) = plot(hg(1),x_pts(ct),var1(ct),'marker','o','markerfacecolor','k','markeredgecolor','k','markersize',12);
 h_point(2) = plot(hg(2),x_pts(ct),var2(ct),'marker','o','markerfacecolor',[0.8 0.2 0.2]);
 h_point(3) = plot(hg(3),x_pts(ct),var3(ct),'marker','o','markerfacecolor',[0.8 0.2 0.2]);
 
@@ -57,19 +60,20 @@ for i = 1:npts
     
     %read in the xray image
     im = imread(fullfile(xray_dir,list_files(i).name));
-    imagesc(him,rot90(im,0))
+    imagesc(him,rot90(rot90(im)))
     drawnow
-    colormap('gray')
+%     colormap('gray')
     axis image
     axis off
     
     
     % set the point to a new current point
-    set(h_point(1),'Xdata',x_pts(ct),'ydata',var1(ct) )
-    xlabel(hg(1),'x-value')
-    ylabel(hg(1),'Sine wave')
-    set(hg(1),'xlim',[min(x_pts)-5, max(x_pts)+5])
-    set(hg(2),'ylim',[min(var3)-5, max(var3)+5])
+    set(h_point(1),'Xdata',x_pts(ct),'ydata',var1(ct))
+    xlabel(hg(1),'% stance')
+    ylabel(hg(1),'')
+    set(hg(1),'xlim',[min(x_pts), max(x_pts)])
+    
+legend(ha(1:2),{'walk','run'})
     
     set(h_point(2),'Xdata',x_pts(ct) ,'ydata',var2(ct) )
     xlabel(hg(2),'x-value')
