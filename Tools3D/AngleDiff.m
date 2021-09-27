12964342bc20a3df23e3e53682616efe8c63b35e
Lauren <welte.lauren@gmail.com>
2021-09-27 16:56:48 -0400

    Deleted the Tools3D toolbox because it is now being hosted by the skelobslab account

--- Tools3D/AngleDiff.m
+++
@@ -1,8 +0,0 @@
-function diff=AngleDiff(vector1,vector2)
-% calculates the angular difference between vector 1 and vector 2
-
-if size(vector1,1)~=size(vector2,1)
-    vector2=vector2';
-end
-
-diff=real(acosd(dot(vector1,vector2,2)./(vnorm(vector1,2).*vnorm(vector2,2))));
--- Tools3D/dist.m
+++
@@ -1,3 +0,0 @@
-function mydist = dist(pt1, pt2)
-
-mydist = (sum((pt1-pt2).^2,2)).^.5;
